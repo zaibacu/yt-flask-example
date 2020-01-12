5fdfa7dbc08f3f6b8f0df7fec6411c644ff14f7f
Šarūnas Navickas <zaibacu@gmail.com>
2020-01-12 12:11:15 +0200

    Make simple APP

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ from routes.health import bp as health_bp
 
 from database import db
 
-app = Flask('example')
 
-app.register_blueprint(admin_bp, url_prefix='/admin')
-app.register_blueprint(site_bp)
-app.register_blueprint(health_bp, url_prefix='/-/')
+def create_app():
+    app = Flask('example')
+    app.register_blueprint(admin_bp, url_prefix='/admin')
+    app.register_blueprint(site_bp)
+    app.register_blueprint(health_bp, url_prefix='/-/')
 
-app.config.from_object(os.getenv('FLASK_CONFIG', 'config.DevConfig'))
+    app.config.from_object(os.getenv('FLASK_CONFIG', 'config.DevConfig'))
 
-db.init_app(app)
+    db.init_app(app)
+    return app
+
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
