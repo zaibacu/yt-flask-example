05c3322764a772c3316f4bc42ef0e7ea0f3fea94
Šarūnas Navickas <zaibacu@gmail.com>
2020-01-12 15:25:06 +0200

    Config for Heroku

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os
+
+
 class BaseConfig(object):
     DEBUG = False
 
@@ -10,3 +13,8 @@ class DevConfig(BaseConfig):
 class ProdConfig(BaseConfig):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'postgres://augczxupdfanck:c1e4912663560b6455696d48b64203742e425aec46219b218806d5a54a48e911@ec2-54-217-234-157.eu-west-1.compute.amazonaws.com:5432/d3hop2juio20u6'
+
+
+class HerokuConfig(BaseConfig):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
